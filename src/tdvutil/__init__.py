24fc2b5745c4182c909af5adcfdafdc2c7381798
TDV Alinsa <alinsa-github@lupine.org>
2024-07-03 14:49:36 -0700

    timefmt: fix missing timecode funcs from top lvl

--- src/tdvutil/__init__.py
+++ src/tdvutil/__init__.py
@@ -3,7 +3,8 @@ from _tdvutil.alintrospect import alintrospect, whatis
 from _tdvutil.now import now, nowf
 from _tdvutil.pathfix import pathfix
 from _tdvutil.ppretty import ppretty
-from _tdvutil.timefmt import hms_to_sec, sec_to_hms, sec_to_shortstr
+from _tdvutil.timefmt import (hms_to_sec, sec_to_hms, sec_to_shortstr,
+                              sec_to_timecode, timecode_to_sec)
 
 __all__ = [
     "__version__",
